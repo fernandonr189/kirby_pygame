59b5d3fc2e79bbbb294a6b15fb283f42e1c579a8
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-03-25 21:50:32 -0600

    Improved kirbys feet

--- pygame_kirby.py
+++ pygame_kirby.py
@@ -33,14 +33,18 @@ def draw_kirby(screen, x, y):
     pg.draw.arc(screen, RED, (x - 15, y + 40, 60, 50), 3.40, -0.15, 6)
 
     # drawing feet
-    left_foot_surface = pg.Surface((110, 160), pg.SRCALPHA, 32);
-    right_foot_surface = pg.Surface((110, 160), pg.SRCALPHA, 32);
+    left_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
+    right_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
 
-    pg.draw.ellipse(left_foot_surface, RED, (0, 0, 110, 160))
-    pg.draw.ellipse(right_foot_surface, RED, (0, 0, 110, 160))
+    pg.draw.ellipse(left_foot_surface, RED, (0, 0, 140, 180))
+    pg.draw.ellipse(right_foot_surface, RED, (0, 0, 140, 180))
 
-    screen.blit(left_foot_surface, (x + 60, y + 100))
-    screen.blit(right_foot_surface, (x - 130, y + 100))
+    left_foot_surface = pg.transform.rotate(left_foot_surface, 30)
+    right_foot_surface = pg.transform.rotate(right_foot_surface, -30)
+
+    screen.blit(left_foot_surface, (x + 40, y + 70))
+    screen.blit(right_foot_surface, (x - 190, y + 70))
+
 
 while running:
     screen.fill((255, 255, 255))
@@ -49,6 +53,7 @@ while running:
             running = False
     draw_kirby(screen, screen.get_width() / 2, screen.get_height() / 2)
     pg.display.flip()
+    print("Hello World!")
     clock.tick(60)
 
 pg.quit()
