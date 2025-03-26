1f2b1a9433ce0ad2d364281969edf780dee00aff
Fernando Ismael Navarrete Ruiz <fernandonr189@outlook.com>
2025-03-25 22:06:20 -0600

    Animated kirbys feet

--- pygame_kirby.py
+++ pygame_kirby.py
@@ -12,12 +12,29 @@ BLACK = (0, 0, 0)
 RED = (255, 100, 100)
 BLUE = (100, 100, 255)
 
+def animate_kirby_feet(screen, x, y, t):
+    left_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
+    right_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
+    pg.draw.ellipse(left_foot_surface, RED, (0, 0, 140, 180))
+    pg.draw.ellipse(right_foot_surface, RED, (0, 0, 140, 180))
+    left_foot_surface = pg.transform.rotate(left_foot_surface, 30 + t)
+    right_foot_surface = pg.transform.rotate(right_foot_surface, -30 - t)
+    screen.blit(left_foot_surface, (x + 40, y + 70))
+    screen.blit(right_foot_surface, (x - 190, y + 70))
+
+def animate_kirby_arms(screen, x, y, t):
+    left_arm_surface = pg.Surface((100, 150), pg.SRCALPHA, 32);
+    right_arm_surface = pg.Surface((100, 150), pg.SRCALPHA, 32);
+    pg.draw.ellipse(left_arm_surface, PINK, (0, 0, 100, 150))
+    pg.draw.ellipse(right_arm_surface, PINK, (0, 0, 100, 150))
+    left_arm_surface = pg.transform.rotate(left_arm_surface, t)
+    right_arm_surface = pg.transform.rotate(right_arm_surface, t)
+    screen.blit(left_arm_surface, (x + 180, y))
+    screen.blit(right_arm_surface, (x - 280, y))
+
 def draw_kirby(screen, x, y):
     # drawing body
     pg.draw.circle(screen, PINK, (x, y), 240)
-    # drawing arms
-    pg.draw.ellipse(screen, PINK, (x + 180, y, 100, 150))
-    pg.draw.ellipse(screen, PINK, (x - 280, y, 100, 150))
 
     # drawing eyes
     pg.draw.ellipse(screen, BLACK, (x + 40, y - 90, 70, 120))
@@ -32,28 +49,20 @@ def draw_kirby(screen, x, y):
     # drawing smile
     pg.draw.arc(screen, RED, (x - 15, y + 40, 60, 50), 3.40, -0.15, 6)
 
-    # drawing feet
-    left_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
-    right_foot_surface = pg.Surface((140, 180), pg.SRCALPHA, 32);
-
-    pg.draw.ellipse(left_foot_surface, RED, (0, 0, 140, 180))
-    pg.draw.ellipse(right_foot_surface, RED, (0, 0, 140, 180))
-
-    left_foot_surface = pg.transform.rotate(left_foot_surface, 30)
-    right_foot_surface = pg.transform.rotate(right_foot_surface, -30)
-
-    screen.blit(left_foot_surface, (x + 40, y + 70))
-    screen.blit(right_foot_surface, (x - 190, y + 70))
-
-
+tx = 0.7
+t = 7
 while running:
     screen.fill((255, 255, 255))
     for event in pg.event.get():
         if event.type == pg.QUIT:
             running = False
     draw_kirby(screen, screen.get_width() / 2, screen.get_height() / 2)
+    animate_kirby_feet(screen, screen.get_width() / 2, screen.get_height() / 2, t)
+    animate_kirby_arms(screen, screen.get_width() / 2, screen.get_height() / 2, (t - 15) / 5)
+    t += tx
+    if t > 15 or t < 5:
+        tx = -tx
     pg.display.flip()
-    print("Hello World!")
     clock.tick(60)
 
 pg.quit()
